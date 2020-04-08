048eb8c32891d4cb47d5920973295c1af8ec7f45
DESKTOP-JRF9H4G\Sabrina <lwy0100@gmail.com>
2020-04-08 11:58:36 +0800

    Update code (multi-thread, cubic traj, parabolic blend)

--- sc_motor_drive.cpp
+++ sc_motor_drive.cpp
@@ -15,21 +15,24 @@ using namespace std;
 using namespace sFnd;
 
 int CheckMotorNetwork();
+void SendMotorCmd(int n);
 void SolveCubicCoef(int loop_i);
+int SolveParaBlend(int loop_i);
 int32_t ToMotorCmd(int motorID, double length);
 
 vector<string> comHubPorts;
 vector<INode*> nodeList; // create a list for each node
 vector<vector<double>> points;
+vector<double> timeout;
 unsigned int portCount;
-const int MILLIS_TO_NEXT_FRAME = 20;   
+const int MILLIS_TO_NEXT_FRAME = 20; // note the basic calculation time is abt 16ms
+const double home[] = {2, 0.5, 2, 0, 0, 0}; // home posisiton
 double in1[6] = {2, 0.5, 2, 0, 0, 0};
-double out1[4];
-double a0, a2, a3, b0, b2, b3, c0, c2, c3, d0, d2, d3, e0, e2, e3, f0, f2, f3;
+double out1[4] = {6.43438, 7.6739, 6.06174, 4.48486};
+double a[6], b[6], c[6], d[6], e[6], f[6], g[6], tb[6]; // trajectory coefficients
 
 int main()
-{h
-
+{
     SysManager* myMgr = SysManager::Instance();
     
     // Start the programme, scan motors in network
@@ -80,49 +83,65 @@ int main()
             }
             points.push_back(row);
         }
-        cout << "Completed reading external input file\n";
+        cout << "Completed reading external input file" << endl;
     }
     
     // Go through the given points
     for (int i = 0; i < points.size(); i++){
         double t = 0;
         // trajectory generation and points splitting
-        SolveCubicCoef(i);
-       
+        // SolveCubicCoef(i);
+        if(SolveParaBlend(i) < 0){ return -3; }
+        cout << a[0] << ", " << b[0] << ", " << c[0] << ", " << d[0] << ", " << e[0] << ", " << f[0] << ", "<< g[0] << ", "<< tb[0] << endl;
+            
         while (t <= points[i][6]){
             auto start = chrono::steady_clock::now();
             long dur = 0;
-            // per time step pose
-            in1[0] = a0 + a2 * t * t + a3 * t * t * t;
-            in1[1] = b0 + b2 * t * t + b3 * t * t * t;
-            in1[2] = c0 + c2 * t * t + c3 * t * t * t;
-            in1[3] = d0 + d2 * t * t + d3 * t * t * t;
-            in1[4] = e0 + e2 * t * t + e3 * t * t * t;
-            in1[5] = f0 + f2 * t * t + f3 * t * t * t;
-            // get absolute cable lengths in meters
-            cout << in1[0] << " " << in1[1] << " " << in1[2] << " " << in1[3] << " " << in1[4] << " " << in1[5] << endl;
-            compile_lengths(in1, out1);
-            cout << out1[0] << " " << out1[1] << " " << out1[2] << " " << out1[3] << endl;
-
-            bool allDone = false;
-            vector<double> timeout;
-            for (int n = 0; n<myPort.NodeCount(); n++) { 
-                // convert to absolute cable length command
-                int32_t step = ToMotorCmd(n, out1[n]);
-                nodeList[n]->Motion.MoveWentDone();
-                nodeList[n]->Motion.MovePosnStart(step, true, true); // absolute position
-                timeout.push_back(nodeList[n]->Motion.MovePosnDurationMsec(step, true)); // absolute position
-                nodeList[n]->Motion.Adv.TriggerGroup(1);
-                cout << "Node " << n << " " << step << "\t Torque: "<< (double)nodeList[n]->Motion.TrqMeasured << endl;
-            }
-            cout << endl;
-            // trigger motors to move
-            myPort.Adv.TriggerMovesInGroup(1);
-            double t_max =  myMgr->TimeStampMsec() + *max_element(timeout.begin(), timeout.end()) + 100;
-            cout << "Estimated time: " << t_max;
-
-            Sleep(MILLIS_TO_NEXT_FRAME); // need some time before sending the next position command? Or else error occurs?
             
+        // per time step pose
+            // CUBIC equation
+            // for (int j = 0; j < 6; j++){
+            //     in1[j] = a[j] + b[j] * t * t + c[j] * t * t * t;
+            // }
+            // PARABOLIC BLEND equation
+            for (int j = 0; j < 6; j++){
+                if (t <= tb[j]){
+                    in1[j] = a[j] + b[j] * t * t;
+                }
+                else if(t <= points[i][6]-tb[j]){
+                    in1[j] = c[j] + d[j];
+                }
+                else{
+                    in1[j] = e[j] + f[j] * t + g[j] * t * t;
+                }
+            }
+            // get absolute cable lengths in meters
+            cout << "IN: "<< in1[0] << " " << in1[1] << " " << in1[2] << " " << in1[3] << " " << in1[4] << " " << in1[5] << endl;
+            // q_initial="2 0.5 2 0 0 0" q_min="0 0 0 -3.1415 -3.1415 -3.1415" q_max="5.0 1.0 5.0 3.1416 3.1416 3.1416"
+            compile_lengths(in1, out1);
+            cout << "OUT: "<<  out1[0] << " " << out1[1] << " " << out1[2] << " " << out1[3] << endl;
+            
+            auto end = chrono::steady_clock::now();
+            dur = chrono::duration_cast<chrono::milliseconds>(end-start).count();
+            cout << "Step to command time: "<< dur << "\t";
+
+            // thread th4(SendMotorCmd,3);
+            // thread th3(SendMotorCmd,2);
+            thread th2(SendMotorCmd,1);
+            thread th1(SendMotorCmd,0);
+            
+            //bool allDone = false;
+            
+            // double t_max =  myMgr->TimeStampMsec() + *max_element(timeout.begin(), timeout.end()) + 100;
+            // cout << "Estimated time: " << t_max;
+            end = chrono::steady_clock::now();
+            dur = chrono::duration_cast<chrono::milliseconds>(end-start).count();
+            cout << " Before sleep: " << dur << endl;
+            
+            // double dif = MILLIS_TO_NEXT_FRAME - dur - 2;
+            // if(dif > 0) { Sleep(dif);}
+            Sleep(MILLIS_TO_NEXT_FRAME);
+
             // if(*max_element(timeout.begin(), timeout.end())>MILLIS_TO_NEXT_FRAME){
             //     cout << "Cannot complete motion in given duration. Exit programme." << endl;
             //     return -1;
@@ -147,9 +166,17 @@ int main()
                     } 
                 }
             }*/
-            auto end = chrono::steady_clock::now();
+
+            // wait sending motor command to complete before triggering motors to move
+            // th4.join();
+            // th3.join();
+            th2.join();
+            th1.join();
+            
+            myPort.Adv.TriggerMovesInGroup(1);
+            end = chrono::steady_clock::now();
             dur = chrono::duration_cast<chrono::milliseconds>(end-start).count();
-            cout << " Time elasped: " << dur << " ; In-loop t: " << t << endl;
+            cout << " Time elasped: " << dur << "\tIn-loop t: " << t << endl;
             timeout.clear();
             t += MILLIS_TO_NEXT_FRAME;
         }
@@ -185,27 +212,42 @@ int main()
         cout << "Velocity set 0\tRequest velocity time: " << dur_p << endl;
     } */
 
+    // Test torque control frequency
+    for (int n = 0; n < nodeList.size(); n++){
+        auto previousT = chrono::steady_clock::now();
+        long dur = 0;
+        double targetTrq = 3;
+        double targetVel;
+        double Kq = 5;
+
+        for (int i = 0; i < 30; i++){
+            nodeList[n]->Motion.TrqCommanded.Refresh();
+			float motorCur = nodeList[n]->Motion.TrqCommanded.Value();
+            targetVel = -Kq * (targetTrq + motorCur);
+            printf("Current size: \t%f \t%f \n", motorCur, targetVel);
+            nodeList[n]->Motion.Adv.MoveVelStart(targetVel);
+            myMgr->Delay(nodeList[n]->Motion.Adv.MoveVelDurationMsec(targetVel));
+            if (motorCur < -15) { //safety measure
+                nodeList[n]->Motion.Adv.MoveVelStart(0);
+                break;
+            }
+
+            // show frequency
+            auto now = chrono::steady_clock::now();
+            dur = chrono::duration_cast<chrono::milliseconds>(now-previousT).count();
+            previousT = now;
+            cout << "Loop time: "<< dur << endl;
+        }
+    }
+    
+    Sleep(6000); // wait a little more before disabling the nodes
     for(int i = 0; i < nodeList.size(); i++){ //Disable Nodes
         myPort.Nodes(i).EnableReq(false);
     }
     nodeList.clear();
     myMgr->PortsClose(); // Close down the ports
     
-    // q_initial="2 0.5 2 0 0 0" q_min="0 0 0 -3.1415 -3.1415 -3.1415" q_max="5.0 1.0 5.0 3.1416 3.1416 3.1416"
-    // compile_lengths(in1, out1);
-
-    /*
-        //At this point we will execute 10 rev moves sequentially on each axis
-        
-        //After moves have completed Disable node, and close ports
-        printf("Disabling nodes, and closing this port\n");
-        for (size_t iNode = 0; iNode < myPort.NodeCount(); iNode++) {
-            myPort.Nodes(iNode).EnableReq(false); //Disable Nodes
-        }
-        nodeList.clear();
-    */
-
-    cout << "\n" << "Done" << endl;
+    cout << "\n" << "Done at " << myMgr->TimeStampMsec() << endl;
     return 0;
 }
 
@@ -250,7 +292,8 @@ int CheckMotorNetwork() {
             theNode.AccUnit(INode::RPM_PER_SEC);        //Set the units for Acceleration to RPM/SEC
             theNode.VelUnit(INode::RPM);                //Set the units for Velocity to RPM
             theNode.Motion.AccLimit = 40000;           //100000 Set Acceleration Limit (RPM/Sec)
-            theNode.Motion.VelLimit = 2500;             //700 Set Velocity Limit (RPM)
+            theNode.Motion.NodeStopDecelLim = 5000;
+            theNode.Motion.VelLimit = 3000;             //700 Set Velocity Limit (RPM)
             theNode.Info.Ex.Parameter(98, 1);           //enable interrupting move
             cout << "AccLimit and VelLimit set" << endl;
 
@@ -270,43 +313,59 @@ int CheckMotorNetwork() {
 }
 
 void SolveCubicCoef(int loop_i){
-    float sA1, sA2, sA3, sA4, sA5, sA6, A1, A2, A3, A4, A5, A6;
-    double home[] = {2, 0.5, 2, 0, 0, 0}; // home posisiton
+    double sQ[6], Q[6];
+    double dura = points[loop_i][6];
     // is it necessary to know the current position?
     
-    sA1 = loop_i == 0 ? home[0] : points[loop_i - 1][0];
-    sA2 = loop_i == 0 ? home[1] : points[loop_i - 1][1];
-    sA3 = loop_i == 0 ? home[2] : points[loop_i - 1][2];
-    sA4 = loop_i == 0 ? home[3] : points[loop_i - 1][3];
-    sA5 = loop_i == 0 ? home[4] : points[loop_i - 1][4];
-    sA6 = loop_i == 0 ? home[5] : points[loop_i - 1][5];
-    A1 = points[loop_i][0];
-    A2 = points[loop_i][1];
-    A3 = points[loop_i][2];
-    A4 = points[loop_i][3];
-    A5 = points[loop_i][4];
-    A6 = points[loop_i][5];
+    for(int i = 0; i < 6; i++){
+        sQ[i] = loop_i == 0 ? home[i] : points[loop_i - 1][i];
+        Q[i] = points[loop_i][i];
+            
+        // solve coefficients of equations for cubic
+        a[i] = sQ[i];
+        b[i] = 3 / (dura * dura) * (Q[i] - sQ[i]);
+        c[i] = -2 / (dura * dura * dura) * (Q[i] - sQ[i]);
+    }
+}
 
-    // solve coefficients of equations for cubic
-    double d = points[loop_i][6];
-    a0 = sA1;
-    a2 = 3 / (d * d) * (A1 - sA1);
-    a3 = -2 / (d * d * d) * (A1 - sA1);
-    b0 = sA2;
-    b2 = 3 / (d * d) * (A2 - sA2);
-    b3 = -2 / (d * d * d) * (A2 - sA2);
-    c0 = sA3;
-    c2 = 3 / (d * d) * (A3 - sA3);
-    c3 = -2 / (d * d * d) * (A3 - sA3);
-    d0 = sA4;
-    d2 = 3 / (d * d) * (A4 - sA4);
-    d3 = -2 / (d * d * d) * (A4 - sA4);
-    e0 = sA5;
-    e2 = 3 / (d * d) * (A5 - sA5);
-    e3 = -2 / (d * d * d) * (A5 - sA5);
-    f0 = sA6;
-    f2 = 3 / (d * d) * (A6 - sA6);
-    f3 = -2 / (d * d * d) * (A6 - sA6);
+int SolveParaBlend(int loop_i){
+    // make them accessable from outside??
+    float vMax[6] = {.01, .001, .01, 0.8, 0.8, 0.8}; // Define the maximum velocity for each DoF
+    float aMax[6] = {.01, .01, .01, .01, .01, .01}; // Define the maximum acceleration for each DoF
+    double sQ[6], Q[6], o[6];
+    double dura = points[loop_i][6];
+    
+    for(int i = 0; i < 6; i++){
+        sQ[i] = loop_i == 0 ? home[i] : points[loop_i - 1][i];
+        Q[i] = points[loop_i][i];
+        tb[i] = dura - (Q[i] - sQ[i]) / vMax[i];
+        if(tb[i] < 0) {
+            cout << "WARNING: Intended trajectory exceeds velocity limit in DoF "<< i << ".\n";
+            return -1;
+        }
+        else if (tb[i] > dura / 2){
+            cout << "ATTENTION: Trajectory for DoF " << i << " will be in cubic form.\n";
+            tb[i] = dura / 2;
+            vMax[i] = (Q[i] - sQ[i]) / dura;
+        }
+        o[i] = vMax[i] / 2 / tb[i];
+        if(abs(o[i]*2) > aMax[i]){
+            cout << "WARNING: Intended trajectory exceeds acceleration limit in DoF "<< i << ".\n";
+            return -1;
+        }
+        
+        // solve coefficients of equations for parabolic
+        a[i] = sQ[i];
+        b[i] = o[i];
+
+        c[i] = sQ[i] - vMax[i] * tb[i] / 2;
+        d[i] = vMax[i];
+
+        e[i] = Q[i] - o[i] * dura * dura;
+        f[i] = 2 * o[i] * dura;
+        g[i] = -o[i];
+    }
+    return 0;
 }
 
 int32_t ToMotorCmd(int motorID, double length){
@@ -330,4 +389,17 @@ int32_t ToMotorCmd(int motorID, double length){
         break;
     }
     return (length + offset) * scale;
+}
+
+void SendMotorCmd(int n){
+    // convert to absolute cable length command
+    int32_t step = ToMotorCmd(n, out1[n]);
+    nodeList[n]->Motion.MoveWentDone();
+    nodeList[n]->Motion.MovePosnStart(step, true, true); // absolute position
+    timeout.push_back(nodeList[n]->Motion.MovePosnDurationMsec(step, true)); // absolute position
+    nodeList[n]->Motion.Adv.TriggerGroup(1);
+    cout << "Node " << n << " " << step << "\tTorque: "<< (double)nodeList[n]->Motion.TrqMeasured;
+    cout << "\tPosition error: " << ((double)nodeList[n]->Motion.PosnMeasured-step) << endl;
+
+    // should we put some saftely factor on tracking position error? set a flag
 }
